3ec0a75528295fb0602145c8cdb1b7d2f890bcf0
CJ Ogbuehi <fahzee1@gmail.com>
2014-05-27 08:34:49 -0500

    add profile controller

---
+++ Prototype/UserProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserProfileViewController.h
+//  Captify
+//
+//  Created by CJ Ogbuehi on 5/27/14.
+//  Copyright (c) 2014 CJ Ogbuehi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserProfileViewController : UIViewController
+
+@end
